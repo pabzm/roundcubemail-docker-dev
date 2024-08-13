9d7c79ada655cbea74829853a642d232eb1d6656
Pablo Zmdl <pablo@nextcloud.com>
2024-08-13 10:27:28 +0200

    Remove minification of JS and CSS
    It's technically not necessary and takes too much time.
    
    Minified files can cause problems when debugging, because they are
    preferred to their non-minified siblings – so if you change a file and
    don't see the result in the browser, check if a minified version of the
    edited file is present and loaded instead.

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -7,8 +7,6 @@ if [[ -f /var/www/html/index.php ]]; then
 	composer --prefer-dist --no-dev --no-interaction --optimize-autoloader install
 	# Download external Javascript dependencies.
 	bin/install-jsdeps.sh
-	# Minify all JS files
-	bin/jsshrink.sh
 	# Translate elastic's styles to CSS.
 	cd skins/elastic
 	npx lessc --clean-css="--s1 --advanced" styles/styles.less > styles/styles.min.css
@@ -17,8 +15,6 @@ if [[ -f /var/www/html/index.php ]]; then
 	cd -
 	# Update cache-buster parameters in CSS-URLs.
 	bin/updatecss.sh
-	# Minify all CSS files.
-	bin/cssshrink.sh
 fi
 
 exec /docker-entrypoint.sh "$@"
